ebadc091e92adb28646b3ffed87f517e0fa42b27
Guangyu Wang <guangyu.wangg@yahoo.com>
2015-06-17 19:56:10 -0400

    [GRAY]: changed color for menu

--- nummy_dev_client_ios/MyNavigationTableViewController.swift
+++ nummy_dev_client_ios/MyNavigationTableViewController.swift
@@ -23,7 +23,7 @@ class MyNavigationTableViewController: UITableViewController {
             red: CGFloat((color & 0xFF0000) >> 16) / 255.0,
             green: CGFloat((color & 0x00FF00) >> 8) / 255.0,
             blue: CGFloat(color & 0x0000FF) / 255.0,
-            alpha: CGFloat(0)
+            alpha: CGFloat(0.5)
         )
         tableView.scrollsToTop = false
         
