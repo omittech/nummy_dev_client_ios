44a508a27a4f344a0e047a73f600580402914039
l7wang <ralph9989@gmail.com>
2015-06-30 00:10:10 -0400

    fix the login issue

--- nummy_dev_client_ios/OrderDetailViewController.swift
+++ nummy_dev_client_ios/OrderDetailViewController.swift
@@ -9,6 +9,33 @@
 import Foundation
 
 
-class OrderDetailViewController: UIViewController {
+class OrderDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var itemsFromChef = [ItemVO]()
+    var orderId: String?
+    
+    @IBOutlet var orderDetailList: UITableView!
+    
+    @IBAction func cancelOder(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        orderDetailList.allowsSelection = false
+        
+        for itemId in shoppingCartVO.itemIds {
+            var item = shoppingCartVO.items[itemId]!
+            if item.restaurantId == chefId {
+                itemsFromChef.append(item)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
---
+++ nummy_dev_client_ios/OrderItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  OrderItemCell.swift
+//  nummy_dev_client_ios
+//
+//  Created by Ralph Wang on 2015-06-28.
+//  Copyright (c) 2015 omittech. All rights reserved.
+//
+
+import UIKit
+class OrderItemCell: UITableViewCell {
+
+    @IBOutlet var itemImage: UIImageView!
+    
+    @IBOutlet var itemName: UILabel!
+    
+    @IBOutlet var itemPrice: UILabel!
+    
+    @IBOutlet var itemQuantity: UITextField!
+
+    
+}
